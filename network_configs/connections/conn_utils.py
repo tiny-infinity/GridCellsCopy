fe65888df3b4c270a918bfdafb55d6ce36431265
inayath-sh <inayath.edu@gmail.com>
2024-12-24 16:54:32 +0530

    Add documentation for connectivity configuration functions in conn_utils.py

--- network_configs/connections/conn_utils.py
+++ network_configs/connections/conn_utils.py
@@ -5,6 +5,17 @@ import os
 import importlib
 
 def file_args():
+    """Creates an argument parser for connectivity configuration.
+
+    Returns:
+        argparse.ArgumentParser: The argument parser with the following arguments:
+            - -i, --sim_id (str, optional): Simulation ID.
+            - -c, --conn_id (str, optional): Connection ID.
+            - -t, --sim_type (str, optional): Simulation type. Default is "single".
+            - -n, --sim_num (int, optional): Simulation number. Default is 0.
+            - -p, --specs_file (str, optional): Path to the specifications file.
+    """
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--sim_id",
                         metavar='N',
@@ -40,10 +51,41 @@ def file_args():
     return parser
 
 def add_project_to_sys_path():
+    """Adds the project's root directory to the system path.
+
+    Python interpreter does not allow importing modules from parent directories.
+    This function resolves the path of the current file, navigates two levels up 
+    to reach the project's root directory, and inserts this directory at the 
+    beginning of the system path. This allowes importing helper_functions, 
+    param files, specs_file etc.
+
+    """
+    
     directory=Path(__file__).resolve().parents[2]
     sys.path.insert(0, str(directory))
 
-def find_params(args):
+
+def find_params(args: argparse.Namespace) -> dict:
+    """Finds and returns simulation parameters based on the provided arguments.
+    
+    Parameters:
+        args (argparse.Namespace): The arguments namespace containing either a 
+        simulation ID or a specifications file.
+    Returns:
+        dict: A dictionary containing the simulation parameters.
+    Raises:
+        FileNotFoundError: If neither sim_id nor specs_file is provided, or if 
+        the specified files are not found.
+    Notes:
+        Parameter dict is generated based on following order of precedence:
+        * If `args.sim_id` is provided, it attempts to read the parameters from 
+            a JSON file in the cache directory.
+            This is what occurs in a typical simulation run.
+        * If the JSON file is not found, it loads the simulation parameters from 
+            data directories using the provided simulation ID.
+        * If `args.specs_file` is provided, it manually generates params dictionary.
+    """
+
     import sim_hf as s_hf
     if args.sim_id:
         try:
