8a5676c8331f6acec44345f4ecd1546117a66b07
inayath-sh <inayath.edu@gmail.com>
2025-01-01 20:57:57 +0530

    add doc for network init

--- network_init.py
+++ network_init.py
@@ -7,20 +7,33 @@ import importlib
 
 
 def network_intialize(params):
-    
-    if params['build_conn_matrx']:
+    """Initialize network and setup instrumentation
+
+    Parameters:
+        params : dict
+            Parameter dictionary
+
+    Returns:
+        network 
+            Network object that includes cells and instrumentations
+
+    """
+
+    #if asked to build matrix, load from cache. Otherwise load a previously saved matrix.
+    if params['build_conn_matrix']:
         file = h5py.File(
-            f"cache/networks/matrix_{params['connect_id']}_{params['sim_id']}_{params['sim_num']}.hdf5", "r"
+            f"cache/matrix_{params['conn_id']}_{params['sim_id']}_{params['sim_num']}.hdf5", "r"
         )
         adj_matrix = file["matrix"]
     else:
         file = h5py.File(
-            f"network_configs/connections/saved_matrices/matrix_{params['connect_id']}.hdf5", "r"
+            f"network_configs/connections/saved_matrices/matrix_{params['conn_id']}.hdf5", "r"
         )
         adj_matrix = file["matrix"]
     network = Network(0, adj_matrix, params)  # initialize grid cells
     file.close()
     
-    setup_instrumentation = importlib.import_module(f"network_configs/instrumentations/{params['instr_id']}_instr.py").setup_instrumentation
+    #Add instrumentation
+    setup_instrumentation = importlib.import_module(f"network_configs.instrumentations.{params['instr_id']}_instr").setup_instrumentation
     setup_instrumentation(network)
     return network
