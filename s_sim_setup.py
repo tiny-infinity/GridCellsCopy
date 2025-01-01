c1e61272cb164894fb48a99e1fb093ab8c22a4d0
inayath-sh <inayath.edu@gmail.com>
2025-01-01 21:02:26 +0530

    debug s_sim_setup

--- s_sim_setup.py
+++ s_sim_setup.py
@@ -8,7 +8,6 @@ import logging
 import sim_hf as s_hf
 from param import Param
 
-
 tstart = time.perf_counter()
 args = s_hf.sim_setup_arg_parser().parse_args()
 #Process specs file
@@ -24,12 +23,12 @@ input_params = param_file.generate_input_params()
 params = Param().update_input_params(input_params)
 params.save_curr_time()
 sim_id = params["sim_id"]
-n_cpus = params["n_cpus"] if params["n_cpus"] else os.cpu_count()//2
+n_cpus = 3 #params["n_cpus"] if params["n_cpus"] else os.cpu_count()//2
 
 #start logger
 logging.basicConfig(handlers=[logging.FileHandler(f"logs/setup_{sim_id}.log"),
-        logging.StreamHandler()], encoding='utf-8', level=logging.DEBUG,
-        format=f'%(asctime)s:%(levelname)s:{sim_id}: %(message)s')
+        logging.StreamHandler()], encoding='utf-8', level=logging.INFO,
+        format=f'%(asctime)s:%(levelname)s: {sim_id}: %(message)s')
 
 # save parameters to cache for running the simulation
 s_hf.json_save(params,f"cache/s_params_merged_{sim_id}.json")
@@ -56,39 +55,41 @@ os.environ["NEURON_MODULE_OPTIONS"] = "-nogui" #Stops no gui warnings in output 
 
 
 #build matrix
-logging.debug(f"Building connectivity matrix")
+logging.info(f"Building connectivity matrix")
 
 t2 = time.perf_counter()
 if params["build_conn_matrix"]:
-    cmd = f"python networks/connections/{params['conn_id']}_conf.py -i {sim_id} -s {params["split_matrx"]}"
+    cmd = f"python network_configs/connections/{params['conn_id']}_config.py -i {sim_id} -s {params["split_built_conn_matrix"]}"
     proc=subprocess.run(cmd.split(),check=True)
-logging.debug(f"Connectivity matrix built in {round(time.perf_counter()-t2,2)}s. saved in network_configs/connections/saved_matrices/")
+logging.info(f"Connectivity matrix built in {round(time.perf_counter()-t2,2)}s")
 
 #run simulation
-# logging.debug(f"Starting simulation")
-# if params["split_sim"][0]:
-#     proc=subprocess.run(["mpiexec","-n", f"{n_cpus}","python", "s_run_split.py",f"{sim_id}"],check=True)
-# else:
-#     proc=subprocess.run(
-#         ["mpiexec","-n", f"{n_cpus}","python", "s_run.py"],check=True)
+logging.debug(f"launching s_run")
+if params["split_sim"][0]:
+    proc=subprocess.run(["mpiexec","-n", f"{n_cpus}","python", "s_run_split.py",f"{sim_id}"],check=True)
+else:
+    proc=subprocess.run(
+        ["mpiexec","-n", f"{n_cpus}","python", "s_run.py", "--sim_id",f"{sim_id}"],check=True)
 
 
-# # save simulation time
-# t_simulation=round(time.perf_counter() - tstart, 2)
-# params["t_simulation"]= t_simulation
+# save simulation time
+t_simulation=round(time.perf_counter() - tstart, 2)
+params["t_simulation"]= t_simulation
 
 
-# #remove cache file
-# try:
-#     os.remove(f"cache/s_params_merged_{sim_id}.json")
-#     os.remove(f"cache/conn_matrix_{sim_id}.json")
+#remove cache file
+try:
+    os.remove(f"cache/s_params_merged_{sim_id}.json")
+except FileNotFoundError as err:
+    logging.warning(f"Could not remove params file cache")
 
-# except:
-#     warnings.warn("Could not remove cache file")
-#     logging.warning(f"Could not remove cache file")
-        
-# # copy params file to data for future reference
-# param_fname = data_loc + "{}.json".format(params['sim_id'])
-# s_hf.json_save(params, param_fname)
+try:
+    os.remove(f"cache/matrix_{params["conn_id"]}_{sim_id}_0.hdf5")
+except FileNotFoundError as err:
+    logging.warning(f"Could not remove matrix file cache")
+            
+# copy params file to data for future reference
+param_fname = data_loc + f"{params['sim_id']}.json"
+s_hf.json_save(params, param_fname)
 
-# logging.INFO(f"Total time (s): {round(time.perf_counter() - tstart, 2)}")
+logging.info(f"Total time (s): {round(time.perf_counter() - tstart, 2)}")
