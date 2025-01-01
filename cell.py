b7c55d4bdd92dc197a6af75cb90c924748e3b34c
inayath-sh <inayath.edu@gmail.com>
2025-01-01 20:32:59 +0530

    add cells

---
+++ cell.py
@@ -0,0 +1,18 @@
+from neuron import h
+#Parent Class for all cells
+
+class Cell:
+    def __init__(self,gid):
+        self._gid = gid               #global id of the cell
+        self._set_morphology()
+        self.all = self.soma.wholetree()
+        self._set_biophysics()
+        self._default_instrumentation()
+
+        #vectors to record spike times and membrane potential
+        self._spike_detector = h.NetCon(self.soma(0.5)._ref_v, None, sec=self.soma)
+        self._ncs = []
+
+    def __repr__(self):
+        return '{}[{}]'.format(self.name, self._gid)
+    
