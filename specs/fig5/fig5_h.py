9dab7610eab8ae8f78cacef95711ef25281e1384
inayath-sh <inayath.edu@gmail.com>
2025-01-17 14:28:06 +0530

    update fig 5 net sim

--- specs/fig5/fig5_h.py
+++ specs/fig5/fig5_h.py
@@ -9,6 +9,7 @@ def generate_input_params():
         "sim_id": "VALD-PRED-NET-S-s-1a",
         "vel_type": "PRED-IHD",
         "save_conn_matrix":True,
+        "split_sim":[True,8000],
         "matrix_id":"prosp",
         "g_h_bar":0,
         "intrnrn_init_noise":[100,0,0.5],
@@ -18,13 +19,13 @@ def generate_input_params():
         "intrnrn_noise": [sim_dur,0,0],
         "allothetic_dur":3000,
         "conn_id":"asym", 
-        "init_noise_seed":np.random.randint(20000),
+        "init_noise_seed":100,
         "si_asym_factor":[1,1],
         "is_asym_factor":is_asym_fact,
         "is_peak_asym_fact":1,
         "is_mean_asym_factor":0,
-        "recorder_dt":0.25,
-        "extra_params":{"stell_dc":0.0019,"dir_change_t":40000},
+        "recorder_dt":0.1,
+        "extra_params":{"stell_dc":0.0015,"dir_change_t":40000},
         "record_handle_stell":{"stell_syn_inhib_g":{"state": True,"cells_to_record":"all"}}
 
     }
