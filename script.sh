5b117dce882b160e2b45c53917f351ad4249551e
tiny-infinity <govind.rnair@students.iiserpune.ac.in>
2025-10-19 22:34:05 +0530

    19/10

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Starting simulation on 18th Oct, 1:45pM"
+echo "Starting simulation on 19th Oct, 12:15pM"
 
 echo "hello how are you, this is script1"
-python m_sim_setup.py specs/theta_mod/NEW_THETA.py -o
+python m_sim_setup.py specs/theta_mod/NEW_THETA_2.py -o
 
 echo "hello how are you, this is script2"
-python m_sim_setup.py specs/theta_mod/NEW_THETA_1.py -o
+python m_sim_setup.py specs/theta_mod/NEW_THETA_3.py -o
 
 
 
