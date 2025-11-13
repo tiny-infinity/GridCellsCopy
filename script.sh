bb8a3071dd6de7d1c3ded7fa7cb4d0df2b6dedd5
tiny-infinity <govind.rnair@students.iiserpune.ac.in>
2025-11-13 17:22:14 +0530

    Plotted error vs amplitude

--- script.sh
+++ script.sh
@@ -3,28 +3,16 @@
 echo "Starting simulation on 13th Nov, 12:46AM"
 
 echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA/1e3.py -o
-
-
-echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA/3e3.py -o
+python m_sim_setup.py specs/RA_2/1e3.py -o
 
 echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA_1/2e3.py -o
+python m_sim_setup.py specs/RA_1/7e3.py -o
 
 echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA_1/3e3.py -o
+python m_sim_setup.py specs/RA_2/7e3.py -o
 
 echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA_1/4e3.py -o
-
-echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA_1/5e3.py -o
-
-echo "hello how are you, this is script1"
-python m_sim_setup.py specs/RA_1/control2.py -o
-
-
+python m_sim_setup.py specs/RA_1/6e3.py -o
 
 
 echo "done"
